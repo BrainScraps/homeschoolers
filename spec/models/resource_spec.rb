5b3e2b36d676fd76342ea0e781104ef721f3bd1d
Patsy Price <patsy.m.price@gmail.com>
2013-08-09 15:10:34 -0700

    added factory girl and generate rspec:install

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Resource do
+  before do
+
   pending "add some examples to (or delete) #{__FILE__}"
+end
+
+
+before do
+  @reservation = FactoryGirl.create(:reservation)
+  @flight = FactoryGirl.create(:flight)
+  @quarter = FactoryGirl.create(:quarter)
+  @weekday = FactoryGirl.create(:weekday)
 end
